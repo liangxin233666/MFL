219f31bfea8cc02464f790dc4f94694c086aa957
liangxin233666 <2762780432@qq.com>
2025-09-06 12:22:13 +0800

    1

---
+++ src/main/java/io/github/liangxin233666/mfl/MflApplication.java
@@ -0,0 +1,13 @@
+package io.github.liangxin233666.mfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MflApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/liangxin233666/mfl/MflApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.liangxin233666.mfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
